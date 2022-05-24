275923b7832e3ee7f88dbfac3f5f195a001dc626
Herman Slatman <hermanslatman@hotmail.com>
2022-05-24 16:39:01 +0200

    Return empty root certificate pool on js/wasm

---
+++ x509/root_js.go
@@ -0,0 +1,13 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build js
+// +build js
+
+package x509
+
+func loadSystemRoots() (*CertPool, error) {
+	roots := NewCertPool()
+	return roots, nil
+}
